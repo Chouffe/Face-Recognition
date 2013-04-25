dbe6e20902cc0652cb883b223e12bbf20e442420
Luis Fernando Reina García-salas <lreina@kth.se>
2013-04-25 09:52:40 +0200

    VexBoxSum done!
    Deleted some useless comments on LoadIm

--- LoadIm.m
+++ LoadIm.m
@@ -1,16 +1,12 @@
 function [im, ii_im]= LoadIm(im_fname)
 %Read the image    
 I = double(imread(im_fname));
+
 % Illummination invariance
 u = mean(I(:));
 o = std(I(:));
 im = (I-u)/o;
-[x,y]= size(im);
-%ii_im = zeros(size(im));
+
+% Cumulative sum. First in y and then in x.
 ii_im = cumsum(cumsum(im,2));
-% for i=1:x
-%    for j=1:y
-%        ii_im(i,j)= cumsum( im(1:i,1:j) );
-%    end
-% end
 end

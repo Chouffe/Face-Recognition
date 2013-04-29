17ba598b37c729d13f75ca5c248fd1974c57c57f
Luis Fernando Reina García-salas <lreina@kth.se>
2013-04-29 17:52:15 +0200

    Still not working Prgm 19
    Just uncommented the Cparams so it could use the new data :P

--- TaskII/Debug.m
+++ TaskII/Debug.m
@@ -81,6 +81,6 @@
 
 dinfo6 = load('../DebugInfo/debuginfo6.mat');
 T = dinfo6.T;
-%Cparams = BoostingAlg(Fdata, NFdata, FTdata, T);
+Cparams = BoostingAlg(Fdata, NFdata, FTdata, T);
 sum(abs(dinfo6.alphas - Cparams.alphas')>eps)
 sum(abs(dinfo6.Thetas(:)  - Cparams.Thetas(:))>eps)

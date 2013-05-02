82fd82f3c4ad432bea6e0995ae40142ed98e7894
Luis Fernando Reina García-salas <lreina@kth.se>
2013-05-02 10:05:00 +0200

    Added to debbug test images

--- TaskIII/Debug.m
+++ TaskIII/Debug.m
@@ -1,7 +1,27 @@
 % Debug file
 
 % Program 20
-Cparams = load('../TaskII/Cparams.mat').Cparams;
+Cparams = load('../TaskII/Cparams.mat');
+Cparams = Cparams.Cparams;
 
+
+% ----------------------------------
+% Program 20 Debug
+% ----------------------------------
 [im, ii_im] = LoadIm('../TrainingImages/FACES/face00001.bmp');
 sc = ApplyDetector(Cparams, ii_im)
+
+
+% ----------------------------------
+% Program 21 Debug
+% ----------------------------------
+% Get the test images.
+face_fnames = dir(Fdata.dirname);
+index = 1:length(face_fnames);
+face_test_index = setdiff(index,Fdata.fnums);
+length(face_test_index)
+% To access the test images we do
+% Choose filename
+% i is the file number
+i=1;
+im_fname = [Fdata.dirname, face_fnames(face_test_index(i)).name];

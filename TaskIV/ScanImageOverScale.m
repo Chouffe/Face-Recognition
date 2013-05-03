b89920d89469079e4843ce43f8fdc2559651df4e
Arthur Caillau <caillau@csc.kth.se>
2013-05-03 17:21:34 +0200

    ScanImageOverScale ready to be tested

--- TaskIV/ScanImageOverScale.m
+++ TaskIV/ScanImageOverScale.m
@@ -3,12 +3,15 @@ function dets = ScanImageOverScale(Cparams, im, min_s, max_s, step_s)
 for s = min_s:step_s:max_s
 
     scaled_im = imresize(im, s);
-    s_dets = ScanImageFixedSize(Cparams, im);
+    s_dets = ScanImageFixedSize(Cparams, scaled_im);
     s_fdets = PruneDetections(s_dets);
-    % TODO
-    % Need to convert it on a detection on the original step
-    dets = [dets, s_fdets];
+
+    % Convert the x_scaled and y_scaled into original values
+    dets = [dets, round((1/s) * s_fdets)];
 
 end
 
+% Is it useful here? ---> Yep
 dets = PruneDetections(dets);
+
+end

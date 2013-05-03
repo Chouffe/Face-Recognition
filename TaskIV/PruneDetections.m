40e426906aeb70008774d9fa0fa53a20285e37a4
Arthur Caillau <caillau@csc.kth.se>
2013-05-03 17:10:15 +0200

    Changed and Fixed PruneDetections and DisplayDetections

--- TaskIV/PruneDetections.m
+++ TaskIV/PruneDetections.m
@@ -3,6 +3,7 @@ function fdets = PruneDetections(dets)
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 nd = size(dets,1);
 =======
 nd = size(dets,2);
@@ -13,6 +14,9 @@ nd = size(dets,1);
 =======
 nd = size(dets,2);
 >>>>>>> Added PruneDetections
+=======
+nd = size(dets,1);
+>>>>>>> Changed and Fixed PruneDetections and DisplayDetections
 D = zeros(nd, nd);
 
 for i = 1:nd
@@ -20,12 +24,16 @@ for i = 1:nd
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> Changed and Fixed PruneDetections and DisplayDetections
 =======
 >>>>>>> Changed and Fixed PruneDetections and DisplayDetections
     xi = dets(i, 1);
     yi = dets(i, 2);
     wi = dets(i, 3);
     hi = dets(i, 4);
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
 =======
@@ -40,6 +48,8 @@ for i = 1:nd
 >>>>>>> Changed and Fixed PruneDetections and DisplayDetections
 =======
 >>>>>>> Added PruneDetections
+=======
+>>>>>>> Changed and Fixed PruneDetections and DisplayDetections
     A = [xi, yi, wi, hi];
 
     for j = 1:nd
@@ -47,12 +57,16 @@ for i = 1:nd
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> Changed and Fixed PruneDetections and DisplayDetections
 =======
 >>>>>>> Changed and Fixed PruneDetections and DisplayDetections
         xj = dets(j, 1);
         yj = dets(j, 2);
         wj = dets(j, 3);
         hj = dets(j, 4);
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
 =======
@@ -67,6 +81,8 @@ for i = 1:nd
 >>>>>>> Changed and Fixed PruneDetections and DisplayDetections
 =======
 >>>>>>> Added PruneDetections
+=======
+>>>>>>> Changed and Fixed PruneDetections and DisplayDetections
         B = [xj, yj, wj, hj];
 
         if rectint(A, B)
@@ -109,12 +125,14 @@ for k = 1:length(C)
 =======
 [S, C] = graphconncomp(sparse(D))
 
-fdets = zeros(4, S);
+fdets = zeros(S, 4);
 total = zeros(S);
 
 for k = 1:length(C)
+
     total(C(k)) = total(C(k)) + 1;
 
+<<<<<<< HEAD
 >>>>>>> Added PruneDetections
     fdets(1, C(k)) = fdets(1, C(k)) + dets(1, C(k));
     fdets(2, C(k)) = fdets(2, C(k)) + dets(2, C(k));
@@ -123,13 +141,18 @@ for k = 1:length(C)
 <<<<<<< HEAD
 >>>>>>> Added PruneDetections
 =======
+=======
+>>>>>>> Changed and Fixed PruneDetections and DisplayDetections
     fdets(C(k), 1) = fdets(C(k), 1) + dets(C(k), 1);
     fdets(C(k), 2) = fdets(C(k), 2) + dets(C(k), 2);
     fdets(C(k), 3) = fdets(C(k), 3) + dets(C(k), 3);
     fdets(C(k), 4) = fdets(C(k), 4) + dets(C(k), 4);
+<<<<<<< HEAD
 >>>>>>> Changed and Fixed PruneDetections and DisplayDetections
 =======
 >>>>>>> Added PruneDetections
+=======
+>>>>>>> Changed and Fixed PruneDetections and DisplayDetections
     
 end
 
@@ -138,6 +161,7 @@ for l = 1:S
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         fdets(l,m) = fdets(l,m) / total(l);
 =======
         fdets(m,l) = fdets(m,l) / total(l);
@@ -148,6 +172,9 @@ for l = 1:S
 =======
         fdets(m,l) = fdets(m,l) / total(l);
 >>>>>>> Added PruneDetections
+=======
+        fdets(l,m) = fdets(l,m) / total(l);
+>>>>>>> Changed and Fixed PruneDetections and DisplayDetections
     end
 end
 

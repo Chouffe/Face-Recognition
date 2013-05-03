b542e3cb4d775812daea693c52bb71bcb967812e
Arthur Caillau <caillau@csc.kth.se>
2013-05-03 15:42:42 +0200

    ScanImageFixedSize Done - need to save threshold in Cparams though

--- TaskIV/ApplyDetectorAdapted.m
+++ TaskIV/ApplyDetectorAdapted.m
@@ -1,9 +1,9 @@
 function sc = ApplyDetectorAdapted(Cparams, patch, sigma, mu, L)
-
-	sc = 0;
-	for t = 1:length(Cparams.alphas)
-
-		% Feature response
+ 
+    sc = 0;
+    for t = 1:length(Cparams.alphas)
+ 
+        % Feature response
         switch Cparams.all_ftypes(Cparams.Thetas(t,1))
             case 3        
                 f = patch(:)' * ((Cparams.fmat(:,Cparams.Thetas(t,1)) + mu * L^2) / sigma);                   
@@ -17,16 +17,17 @@ function sc = ApplyDetectorAdapted(Cparams, patch, sigma, mu, L)
                 f = patch(:)' * (Cparams.fmat(:,Cparams.Thetas(t,1)) / sigma);
         end
         % f = (1 / sigma) * patch(:)' * Cparams.fmat(:,Cparams.Thetas(t,1));
-		% Parity
+        % Parity
         p = Cparams.Thetas(t,3);
         % threshold
-		theta = Cparams.Thetas(t,2);
+        theta = Cparams.Thetas(t,2);
         % score
-		sc = sc + Cparams.alphas(t) * h(f,p,theta);
-	end
+        sc = sc + Cparams.alphas(t) * h(f,p,theta);
+    end
 end
-
+ 
 % Weak classifier.
 function cls = h(f,p,theta)
-	cls = p.*f < p*theta;
+    cls = p.*f < p*theta;
 end
+

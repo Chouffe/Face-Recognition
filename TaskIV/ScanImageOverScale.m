77e03a5982593d6ed6f4de6b1f15c167b5e2d7b4
Arthur Caillau <caillau@csc.kth.se>
2013-05-03 16:52:53 +0200

    Started ScanImageOverScale

--- TaskIV/ScanImageOverScale.m
+++ TaskIV/ScanImageOverScale.m
@@ -1,6 +1,7 @@
 function dets = ScanImageOverScale(Cparams, im, min_s, max_s, step_s)
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> ScanImageOverScale done, part IV done, check that out
 
@@ -64,3 +65,18 @@ end
 
 end
 >>>>>>> ScanImageOverScale ready to be tested
+=======
+    
+for s = min_s:step_s:max_s
+
+    scaled_im = imresize(im, s);
+    s_dets = ScanImageFixedSize(Cparams, im);
+    s_fdets = PruneDetections(s_dets);
+    % TODO
+    % Need to convert it on a detection on the original step
+    dets = [dets, s_fdets];
+
+end
+
+dets = PruneDetections(dets);
+>>>>>>> Started ScanImageOverScale

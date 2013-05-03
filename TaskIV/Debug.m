1777c7a4c190d14f26a6502efa1d03be5d917572
Arthur Caillau <caillau@csc.kth.se>
2013-05-03 15:42:42 +0200

    ScanImageFixedSize Done - need to save threshold in Cparams though

--- TaskIV/Debug.m
+++ TaskIV/Debug.m
@@ -1,3 +1,4 @@
+
 % %Debug%
 % im = '../TestImages/one_chris.png';
 % addpath('../TaskIII');
@@ -13,6 +14,13 @@
 
 %Test
 
+addpath('../TaskIII/');
+addpath('../TaskI/');
+
+% Cparams = load('../TaskII/Cparams_AllT3.mat');
+Cparams = load('../TaskII/Cparams.mat');
+Cparams = Cparams.Cparams;
+
 im = '../TrainingImages/FACES/face00007.bmp';
 [i1,i2] = LoadIm(im);
 a = ApplyDetector(Cparams,i2)
@@ -24,4 +32,3 @@ abs(a-b)
 im = '../TestImages/one_chris.png'
 dets = ScanImageFixedSize(Cparams, im);
 size(find (dets> Cparams.thresh))
-

24e569c77146c11fe4abdfe98b4ac91a7c285357
Arthur Caillau <caillau@csc.kth.se>
2013-05-03 16:10:07 +0200

    DisplayDetections done

---
+++ TaskIV/DisplayDetections.m
@@ -0,0 +1,18 @@
+function DisplayDetections(im, dets)
+
+I = imread(im);
+
+figure, imshow(I);
+detectedFaceNumber = size(dets,2);
+
+for i = 1:detectedFaceNumber
+
+    x = dets(1, i);
+    y = dets(2, i);
+    w = dets(3, i) - x;
+    h = dets(4, i) - y;
+
+    rectangle('Position',[x, y, w, h],'EdgeColor', 'r');
+end
+
+end

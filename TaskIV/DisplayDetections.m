b11b9598fe9b5ac3f91e7ece271a54aff2509b24
Arthur Caillau <caillau@csc.kth.se>
2013-05-03 16:10:07 +0200

    DisplayDetections done

--- TaskIV/DisplayDetections.m
+++ TaskIV/DisplayDetections.m
@@ -1,13 +1,13 @@
 function DisplayDetections(im, dets)
 % just a test
 I = imread(im);
-
-
+ 
+ 
 figure(); imagesc(I); axis equal;
 detectedFaceNumber = size(dets,1);
-
+ 
 for i = 1:detectedFaceNumber
-
+ 
     x = dets(i, 1)-0.5;
     y = dets(i, 2)-0.5;
     w = dets(i, 3)+0.5;
@@ -16,5 +16,6 @@ for i = 1:detectedFaceNumber
     % % Wrong ploting
     % rectangle('Position',[x - w/2, y + h/2, w, h],'EdgeColor', 'b');
 end
-
+ 
 end
+

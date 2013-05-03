b726e23807f45b8e96fa01fc0690e238fd6c76fe
Arthur Caillau <caillau@csc.kth.se>
2013-05-03 17:10:15 +0200

    Changed and Fixed PruneDetections and DisplayDetections

--- TaskIV/PruneDetections.m
+++ TaskIV/PruneDetections.m
@@ -1,22 +1,22 @@
 function fdets = PruneDetections(dets)
     
-nd = size(dets,2);
+nd = size(dets,1);
 D = zeros(nd, nd);
 
 for i = 1:nd
 
-    xi = dets(1, i);
-    yi = dets(2, i);
-    wi = dets(3, i) - xi;
-    hi = dets(4, i) - yi;
+    xi = dets(i, 1);
+    yi = dets(i, 2);
+    wi = dets(i, 3);
+    hi = dets(i, 4);
     A = [xi, yi, wi, hi];
 
     for j = 1:nd
 
-        xj = dets(1, j);
-        yj = dets(2, j);
-        wj = dets(3, j) - xj;
-        hj = dets(4, j) - yj;
+        xj = dets(j, 1);
+        yj = dets(j, 2);
+        wj = dets(j, 3);
+        hj = dets(j, 4);
         B = [xj, yj, wj, hj];
 
         if rectint(A, B)
@@ -28,22 +28,23 @@ end
 
 [S, C] = graphconncomp(sparse(D))
 
-fdets = zeros(4, S);
+fdets = zeros(S, 4);
 total = zeros(S);
 
 for k = 1:length(C)
+
     total(C(k)) = total(C(k)) + 1;
 
-    fdets(1, C(k)) = fdets(1, C(k)) + dets(1, C(k));
-    fdets(2, C(k)) = fdets(2, C(k)) + dets(2, C(k));
-    fdets(3, C(k)) = fdets(3, C(k)) + dets(3, C(k));
-    fdets(4, C(k)) = fdets(4, C(k)) + dets(4, C(k));
+    fdets(C(k), 1) = fdets(C(k), 1) + dets(C(k), 1);
+    fdets(C(k), 2) = fdets(C(k), 2) + dets(C(k), 2);
+    fdets(C(k), 3) = fdets(C(k), 3) + dets(C(k), 3);
+    fdets(C(k), 4) = fdets(C(k), 4) + dets(C(k), 4);
     
 end
 
 for l = 1:S
     for m = 1:4
-        fdets(m,l) = fdets(m,l) / total(l);
+        fdets(l,m) = fdets(l,m) / total(l);
     end
 end
 
